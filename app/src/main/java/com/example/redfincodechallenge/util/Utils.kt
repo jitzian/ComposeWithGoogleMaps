9ea9b321652d0a43da7fd0d3fcbd3af5fd89480a
joNA <jitzian@gmail.com>
2022-07-31 11:04:37 -0400

    comp jc - Adding logic for check open and close hours
    Cleanup is required

--- app/src/main/java/com/example/redfincodechallenge/util/Utils.kt
+++ app/src/main/java/com/example/redfincodechallenge/util/Utils.kt
@@ -1,5 +1,8 @@
 package com.example.redfincodechallenge.util
 
+import android.util.Log
+import com.example.redfincodechallenge.constants.GlobalConstants.Companion.DATE_FORMATTING_AM_PM
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
@@ -18,6 +21,52 @@ fun <T1 : Any, T2 : Any, T3 : Any, R : Any> safeLet(
     block: (T1, T2, T3) -> R?
 ): R? {
     return if (p1 != null && p2 != null && p3 != null) block(p1, p2, p3) else null
+}
+
+fun getCurrentHour(): String {
+    val date = Date()
+    val formatTime = SimpleDateFormat(DATE_FORMATTING_AM_PM, Locale.ROOT)
+    return formatTime.format(date)
+}
+
+fun convertDate(input: String) {
+    /*val displayFormat = SimpleDateFormat("HH:mm")
+    val parseFormat = SimpleDateFormat("hh:mm a")
+    val date = parseFormat.parse("10:30 PM")*/
+    val formatTime = SimpleDateFormat(DATE_FORMATTING_AM_PM, Locale.ROOT)
+    val convertedDate = formatTime.parse(input)
+
+    Log.e("UTIL", "convertDate::$convertedDate")
+}
+
+fun checkAMHours(startTime: String): Boolean {
+    val isAvailable: Boolean
+    val currentHour = getCurrentHour()
+
+    isAvailable = if (currentHour.contains("AM")) {
+        val currentHourAlone = currentHour.substringBefore("AM")
+        val startTimeHourAlone = startTime.substringAfter("AM")
+        currentHourAlone >= startTimeHourAlone
+    } else {
+        true
+    }
+
+    return isAvailable
+}
+
+fun checkPMHours(endTime: String): Boolean {
+    val isAvailable: Boolean
+    val currentHour = getCurrentHour()
+
+    isAvailable = if (currentHour.contains("PM")) {
+        val currentHourAlone = currentHour.substringBefore("PM")
+        val startTimeHourAlone = endTime.substringAfter("PM")
+        currentHourAlone <= startTimeHourAlone
+    } else {
+        true
+    }
+
+    return isAvailable
 }
 
 fun getWeekDay(): Days = when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
