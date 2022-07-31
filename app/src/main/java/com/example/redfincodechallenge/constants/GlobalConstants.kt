05f8bd3e2e60ff6992e7a300f5f97e2a1ab514da
joNA <jitzian@gmail.com>
2022-07-30 22:05:42 -0400

    comp jc - Updating constantans and adding dummy URL for inserting an image

--- app/src/main/java/com/example/redfincodechallenge/constants/GlobalConstants.kt
+++ app/src/main/java/com/example/redfincodechallenge/constants/GlobalConstants.kt
@@ -2,7 +2,9 @@ package com.example.redfincodechallenge.constants
 
 class GlobalConstants {
     companion object {
-        //https://data.sfgov.org/resource/jjew-r69b.json
         const val baseURl = "https://data.sfgov.org/"
+        const val MAX_TIME_OUT = 10000L
+        const val FOOD_TRUCK_IMAGE_URL =
+            "https://images.unsplash.com/photo-1607947500315-dc88678fca88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3432&q=80"
     }
 }
