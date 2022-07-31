2e7ee7b67402f708ecc07e34aea666b4b6358612
joNA <jitzian@gmail.com>
2022-07-31 11:12:29 -0400

    comp jc - Updating constants file with AM/PM time hours

--- app/src/main/java/com/example/redfincodechallenge/constants/GlobalConstants.kt
+++ app/src/main/java/com/example/redfincodechallenge/constants/GlobalConstants.kt
@@ -6,5 +6,10 @@ class GlobalConstants {
         const val MAX_TIME_OUT = 10000L
         const val FOOD_TRUCK_IMAGE_URL =
             "https://images.unsplash.com/photo-1607947500315-dc88678fca88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3432&q=80"
+
+        //Date Formatting
+        const val DATE_FORMATTING_AM_PM = "hhaa"
+        const val AM_BEFORE_NOON = "AM"
+        const val PM_AFTER_NOON = "PM"
     }
 }
