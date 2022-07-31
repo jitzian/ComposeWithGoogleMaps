8dc6bb91b805d09a66b17ac416e9357f9c75420f
joNA <jitzian@gmail.com>
2022-07-30 22:34:38 -0400

    comp jc - Adding utility to get the current day of the week

--- app/src/main/java/com/example/redfincodechallenge/util/Utils.kt
+++ app/src/main/java/com/example/redfincodechallenge/util/Utils.kt
@@ -1,5 +1,10 @@
 package com.example.redfincodechallenge.util
 
+import java.util.*
+
+/**
+ * Extension function for generating TAG for any type of class
+ * */
 inline fun <reified T> T.TAG(): String = T::class.java.simpleName
 
 fun <T1 : Any, T2 : Any, R : Any> safeLet(p1: T1?, p2: T2?, block: (T1, T2) -> R?): R? {
@@ -13,4 +18,38 @@ fun <T1 : Any, T2 : Any, T3 : Any, R : Any> safeLet(
     block: (T1, T2, T3) -> R?
 ): R? {
     return if (p1 != null && p2 != null && p3 != null) block(p1, p2, p3) else null
+}
+
+fun getWeekDay(): Days = when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
+    Calendar.SUNDAY -> {
+        Days.SUNDAY
+    }
+    Calendar.MONDAY -> {
+        Days.MONDAY
+    }
+    Calendar.TUESDAY -> {
+        Days.TUESDAY
+    }
+    Calendar.WEDNESDAY -> {
+        Days.WEDNESDAY
+    }
+    Calendar.THURSDAY -> {
+        Days.THURSDAY
+    }
+    Calendar.FRIDAY -> {
+        Days.FRIDAY
+    }
+    else -> {
+        Days.SATURDAY
+    }
+}
+
+enum class Days(val value: String) {
+    SUNDAY("Sunday"),
+    MONDAY("Monday"),
+    TUESDAY("Tuesday"),
+    WEDNESDAY("Wednesday"),
+    THURSDAY("Thursday"),
+    FRIDAY("Friday"),
+    SATURDAY("Saturday"),
 }
