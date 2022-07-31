9ae3666d9af73e1e4637bb6a75ea9a4fd85eeb66
joNA <jitzian@gmail.com>
2022-07-30 21:51:25 -0400

    comp jc - Creation of generic Error Screen

---
+++ app/src/main/java/com/example/redfincodechallenge/ui/common/ErrorScreen.kt
@@ -0,0 +1,43 @@
+package com.example.redfincodechallenge.ui.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.example.redfincodechallenge.ui.app.RedFinScreen
+import com.example.redfincodechallenge.R
+
+@Composable
+fun ErrorScreen(message: String?) {
+    RedFinScreen {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painterResource(R.drawable.transpatent_owl),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .width(dimensionResource(id = R.dimen.cell_min_width))
+                    .height(dimensionResource(id = R.dimen.cell_min_width))
+                    .padding(dimensionResource(id = R.dimen.dimen_16_dp))
+
+            )
+            Text(
+                text = message ?: stringResource(id = R.string.no_internet_connection_TEXT),
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.dimen_16_dp)),
+                style = MaterialTheme.typography.h5
+            )
+        }
+    }
+}
