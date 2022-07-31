c0f63bf97f41fdfae94e259ee52d882e4347dd0b
joNA <jitzian@gmail.com>
2022-07-31 11:06:24 -0400

    comp jc - Integrating open/close hours and filtering data by that criteria.
    Cleanup is also required

--- app/src/main/java/com/example/redfincodechallenge/mainView/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/redfincodechallenge/mainView/viewmodel/MainViewModel.kt
@@ -1,11 +1,14 @@
 package com.example.redfincodechallenge.mainView.viewmodel
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.viewModelScope
 import com.example.redfincodechallenge.base.BaseViewModel
 import com.example.redfincodechallenge.constants.GlobalConstants
 import com.example.redfincodechallenge.rest.model.ResultApi
-import com.example.redfincodechallenge.util.TAG
+import com.example.redfincodechallenge.rest.model.ResultApiItem
+import com.example.redfincodechallenge.util.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -17,17 +20,37 @@ class MainViewModel : BaseViewModel() {
     val state: StateFlow<UIState>
         get() = _state.asStateFlow()
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun fetchData() = viewModelScope.launch {
+        Log.e(this.TAG(), "fetchData::${getWeekDay()} - ${getCurrentHour()}")
         try {
             withTimeout(GlobalConstants.MAX_TIME_OUT) {
                 withContext(Dispatchers.IO) {
                     if (_state.value == UIState.Loading) {
                         val result = restApi.getData()
-                        Log.e(this.TAG(), "fetchData::result::$result")
+                        Log.e(this.TAG(), "fetchData::result::${result.size}")
                         if (result.isEmpty()) {
                             _state.value = UIState.Error(message = "No data is available")
                         } else {
-                            _state.value = UIState.Success(data = result)
+
+                            val filteredData = result.filter { data ->
+                                safeLet(
+                                    data.dayofweekstr,
+                                    data.starttime,
+                                    data.endtime
+                                ) { dayOfWeekStr, startTime, endTime ->
+                                    dayOfWeekStr == getWeekDay().value && checkAMHours(startTime) && checkPMHours(
+                                        endTime
+                                    )
+                                }!!
+                            }.map {
+                                it
+                            }
+
+                            Log.e(this.TAG(), "fetchData::filteredData::${filteredData.size}")
+
+                            //_state.value = UIState.Success(data = result)
+                            _state.value = UIState.Success(data = filteredData)
                         }
                     }
                 }
@@ -46,7 +69,8 @@ class MainViewModel : BaseViewModel() {
      * */
     sealed class UIState {
         object Loading : UIState()
-        class Success(val data: ResultApi) : UIState()
+        //class Success(val data: ResultApi) : UIState()
+        class Success(val data: List<ResultApiItem>) : UIState()
         class Error(val message: String) : UIState()
     }
 }
