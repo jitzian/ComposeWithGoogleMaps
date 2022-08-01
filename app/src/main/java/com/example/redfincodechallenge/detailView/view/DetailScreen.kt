83ea4a79acdc35de581e597f9b3b851f2b3a262c
joNA <jitzian@gmail.com>
2022-08-01 16:11:25 -0400

    comp jc - First iteration to kinda wire-up the Navigation with the action of passing params to the Detail Screen

--- app/src/main/java/com/example/redfincodechallenge/detailView/view/DetailScreen.kt
+++ app/src/main/java/com/example/redfincodechallenge/detailView/view/DetailScreen.kt
@@ -1,22 +1,31 @@
 package com.example.redfincodechallenge.detailView.view
 
+import android.util.Log
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.redfincodechallenge.mainView.model.Items
+import com.example.redfincodechallenge.mainView.view.MainScreenState
+import com.example.redfincodechallenge.mainView.viewmodel.MainViewModel
+import com.example.redfincodechallenge.rest.model.ResultApiItem
 import com.example.redfincodechallenge.ui.app.RedFinScreen
+import com.example.redfincodechallenge.ui.common.ErrorScreen
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
-@Composable
 @Destination
-fun DetailScreenState() {
-    DetailScreen()
+@Composable
+fun DetailScreenState(items: Items) {
+    Log.e("DetailScreenState", "DetailScreenState: --> $items")
+    DetailScreen(data = items.data)
 }
 
 @Composable
-@Preview(showBackground = true)
-fun DetailScreen() {
+fun DetailScreen(data: List<ResultApiItem>) {
+    Log.e("DetailScreen", "DetailScreen")
     RedFinScreen {
         Scaffold {
             Text(text = "Details")
--- app/src/main/java/com/example/redfincodechallenge/mainView/view/MainScreen.kt
+++ app/src/main/java/com/example/redfincodechallenge/mainView/view/MainScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.redfincodechallenge.destinations.DetailScreenStateDestination
+import com.example.redfincodechallenge.mainView.model.Items
 import com.example.redfincodechallenge.mainView.viewmodel.MainViewModel
 import com.example.redfincodechallenge.rest.model.ResultApiItem
 import com.example.redfincodechallenge.ui.app.RedFinScreen
@@ -35,7 +37,10 @@ fun MainScreenState(
             LoadingScreen()
         }
         is MainViewModel.UIState.Success -> {
-            MainScreen(data = (state as MainViewModel.UIState.Success).data)
+            MainScreen(
+                data = (state as MainViewModel.UIState.Success).data,
+                navigator = navigator
+            )
         }
         is MainViewModel.UIState.Error -> {
             ErrorScreen(message = (state as MainViewModel.UIState.Error).message)
@@ -44,14 +49,20 @@ fun MainScreenState(
 }
 
 @Composable
-fun MainScreen(data: List<ResultApiItem>) {
+fun MainScreen(navigator: DestinationsNavigator, data: List<ResultApiItem>) {
     val state = rememberLazyListState()
+
     RedFinScreen {
         Scaffold {
             LazyColumn(state = state) {
                 items(data) { item ->
                     ItemRow(data = item, onItemSelected = {
                         Log.e(this.TAG(), "MainScreen::CLICKED:: ${item.applicant}")
+                        navigator.navigate(
+                            DetailScreenStateDestination(
+                                Items(data = data)
+                            )
+                        )
                     })
                 }
             }
