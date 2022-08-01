c80c5fe33c9a8fed78f8869812d8fdb6b997a7aa
joNA <jitzian@gmail.com>
2022-08-01 19:54:17 -0400

    comp jc - First iteration for the viewmodel that will create the markers that will populate list of markers in the google maps

---
+++ app/src/main/java/com/example/redfincodechallenge/detailView/viewmodel/DetailViewModel.kt
@@ -0,0 +1,53 @@
+package com.example.redfincodechallenge.detailView.viewmodel
+
+import android.util.Log
+import com.example.redfincodechallenge.base.BaseViewModel
+import com.example.redfincodechallenge.rest.model.ResultApiItem
+import com.example.redfincodechallenge.util.TAG
+import com.example.redfincodechallenge.util.safeLet
+import com.google.android.libraries.maps.model.LatLng
+import com.google.android.libraries.maps.model.MarkerOptions
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class DetailViewModel : BaseViewModel() {
+
+    private val _state = MutableStateFlow<UIState>(UIState.Loading)
+    val state: StateFlow<UIState>
+        get() = _state.asStateFlow()
+
+    fun getListOfMarkers(data: List<ResultApiItem>) {
+        if (_state.value == UIState.Loading) {
+            if (data.isNotEmpty()) {
+                val listOfMarkerOptions = mutableListOf<MarkerOptions>()
+                data.forEach { item ->
+                    safeLet(
+                        item.applicant,
+                        item.latitude,
+                        item.longitude
+                    ) { location, latitude, longitude ->
+
+                        val latLong = LatLng(latitude.toDouble(), longitude.toDouble())
+                        val markerOption = MarkerOptions()
+                            .title(location)
+                            .position(latLong)
+
+                        listOfMarkerOptions.add(markerOption)
+                    }
+                }
+                _state.value = UIState.Success(markers = listOfMarkerOptions)
+            } else {
+                Log.e(this.TAG(), "getListOfMarkers::No markers available")
+                _state.value = UIState.Error("No markers available")
+            }
+        }
+    }
+
+    sealed class UIState {
+        object Loading : UIState()
+        class Success(val markers: List<MarkerOptions>) : UIState()
+        class Error(val message: String) : UIState()
+    }
+
+}
