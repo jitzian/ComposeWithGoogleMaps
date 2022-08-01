dda2ab0d94702ba84b8a3fa733ac996e51973e44
joNA <jitzian@gmail.com>
2022-08-01 16:05:44 -0400

    comp jc - Parcelizing model classes

--- app/src/main/java/com/example/redfincodechallenge/rest/model/Location2.kt
+++ app/src/main/java/com/example/redfincodechallenge/rest/model/Location2.kt
@@ -1,12 +1,11 @@
 package com.example.redfincodechallenge.rest.model
 
-import com.google.gson.annotations.SerializedName
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Location2(
-    @SerializedName("human_address")
     val humanAddress: String?,
-    @SerializedName("latitude")
     val latitude: String?,
-    @SerializedName("longitude")
     val longitude: String?
-)
+): Parcelable
