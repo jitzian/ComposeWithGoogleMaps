8dbf7caac9feb0448945340af51d8f7fa72fe976
joNA <jitzian@gmail.com>
2022-08-01 20:02:31 -0400

    comp jc - Package re-organization

---
+++ app/src/main/java/com/example/redfincodechallenge/detailView/view/UIGoogleMaps.kt
@@ -0,0 +1,92 @@
+package com.example.redfincodechallenge.detailView.view
+
+import android.os.Bundle
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.google.android.libraries.maps.CameraUpdateFactory
+import com.google.android.libraries.maps.MapView
+import com.google.android.libraries.maps.model.MarkerOptions
+import com.google.maps.android.ktx.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@Composable
+fun GoogleMaps(data: List<MarkerOptions>) {
+    val mapView = rememberMapViewWithLifeCycle()
+
+    Box {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            AndroidView({ mapView }) { mapView ->
+                CoroutineScope(Dispatchers.Main).launch {
+
+                    mapView.getMapAsync {
+                        it.mapType = 1
+                        it.uiSettings.isZoomControlsEnabled = true
+
+                        data.forEach { marker ->
+                            it.addMarker(marker)
+                        }
+
+                        it.moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(data.first().position, 12f)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun rememberMapViewWithLifeCycle(): MapView {
+    val context = LocalContext.current
+    val mapView = remember {
+        MapView(context).apply {
+            id = R.id.map_frame
+        }
+    }
+    val lifeCycleObserver = rememberMapLifecycleObserver(mapView)
+    val lifeCycle = LocalLifecycleOwner.current.lifecycle
+    DisposableEffect(lifeCycle) {
+        lifeCycle.addObserver(lifeCycleObserver)
+        onDispose {
+            lifeCycle.removeObserver(lifeCycleObserver)
+        }
+    }
+
+    return mapView
+}
+
+@Composable
+fun rememberMapLifecycleObserver(mapView: MapView): LifecycleEventObserver =
+    remember(mapView) {
+        LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
+                Lifecycle.Event.ON_START -> mapView.onStart()
+                Lifecycle.Event.ON_RESUME -> mapView.onResume()
+                Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+                Lifecycle.Event.ON_STOP -> mapView.onStop()
+                Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
+                else -> throw IllegalStateException()
+            }
+        }
+    }
