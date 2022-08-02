b8bf9cfe32444f54e67087b9f83c0948f844bd6f
joNA <jitzian@gmail.com>
2022-08-01 20:00:51 -0400

    comp jc - First iteration for creating google maps composable
    Cleanup is required

--- app/src/main/java/com/example/redfincodechallenge/detailView/view/DetailScreen.kt
+++ app/src/main/java/com/example/redfincodechallenge/detailView/view/DetailScreen.kt
@@ -1,34 +1,128 @@
 package com.example.redfincodechallenge.detailView.view
 
-import android.util.Log
+import android.os.Bundle
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.redfincodechallenge.detailView.viewmodel.DetailViewModel
 import com.example.redfincodechallenge.mainView.model.Items
-import com.example.redfincodechallenge.mainView.view.MainScreenState
-import com.example.redfincodechallenge.mainView.viewmodel.MainViewModel
-import com.example.redfincodechallenge.rest.model.ResultApiItem
 import com.example.redfincodechallenge.ui.app.RedFinScreen
 import com.example.redfincodechallenge.ui.common.ErrorScreen
+import com.example.redfincodechallenge.ui.common.LoadingScreen
+import com.google.android.libraries.maps.CameraUpdateFactory
+import com.google.android.libraries.maps.MapView
+import com.google.android.libraries.maps.model.MarkerOptions
 import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Destination
 @Composable
-fun DetailScreenState(items: Items) {
-    Log.e("DetailScreenState", "DetailScreenState: --> $items")
-    DetailScreen(data = items.data)
-}
+fun DetailScreenState(items: Items, detailViewModel: DetailViewModel = viewModel()) {
 
-@Composable
-fun DetailScreen(data: List<ResultApiItem>) {
-    Log.e("DetailScreen", "DetailScreen")
-    RedFinScreen {
-        Scaffold {
-            Text(text = "Details")
+    val state by detailViewModel.state.collectAsState()
+    detailViewModel.generateListOfMarkers(items.data)
+
+    when (state) {
+        is DetailViewModel.UIState.Loading -> {
+            LoadingScreen()
+        }
+        is DetailViewModel.UIState.Success -> {
+            DetailScreen(
+                data = (state as DetailViewModel.UIState.Success).markers
+            )
+        }
+        is DetailViewModel.UIState.Error -> {
+            ErrorScreen(message = (state as DetailViewModel.UIState.Error).message)
         }
     }
 }
+
+@Composable
+fun DetailScreen(data: List<MarkerOptions>) {
+    RedFinScreen {
+        Scaffold {
+            GoogleMaps(data)
+        }
+    }
+}
+
+@Composable
+fun GoogleMaps(data: List<MarkerOptions>) {
+    val mapView = rememberMapViewWithLifeCycle()
+
+    Box {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            AndroidView({ mapView }) { mapView ->
+                CoroutineScope(Dispatchers.Main).launch {
+
+                    mapView.getMapAsync {
+                        it.mapType = 1
+                        it.uiSettings.isZoomControlsEnabled = true
+
+                        data.forEach { marker ->
+                            it.addMarker(marker)
+                        }
+
+                        it.moveCamera(
+                            CameraUpdateFactory.newLatLngZoom(data.first().position, 12f)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun rememberMapViewWithLifeCycle(): MapView {
+    val context = LocalContext.current
+    val mapView = remember {
+        MapView(context).apply {
+            id = com.google.maps.android.ktx.R.id.map_frame
+        }
+    }
+    val lifeCycleObserver = rememberMapLifecycleObserver(mapView)
+    val lifeCycle = LocalLifecycleOwner.current.lifecycle
+    DisposableEffect(lifeCycle) {
+        lifeCycle.addObserver(lifeCycleObserver)
+        onDispose {
+            lifeCycle.removeObserver(lifeCycleObserver)
+        }
+    }
+
+    return mapView
+}
+
+@Composable
+fun rememberMapLifecycleObserver(mapView: MapView): LifecycleEventObserver =
+    remember(mapView) {
+        LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
+                Lifecycle.Event.ON_START -> mapView.onStart()
+                Lifecycle.Event.ON_RESUME -> mapView.onResume()
+                Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+                Lifecycle.Event.ON_STOP -> mapView.onStop()
+                Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
+                else -> throw IllegalStateException()
+            }
+        }
+    }
